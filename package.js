170ba5ce5bc570bf641be2f46b19e26d860025e4
uzumaxy <uzumaxy@gmail.com>
2015-08-06 04:14:05 +0300

    Fixed bug with data-picker in Firefox.

--- package.js
+++ package.js
@@ -1,15 +1,17 @@
 Package.describe({
   name: 'gildaspk:autoform-moment-datepicker',
   summary: 'Moment datepicker for AutoForm',
-  version: '0.0.1',
-  git: 'https://github.com/gildaspk/meteor-autoform-moment-datepicker.git'
+  version: '0.0.2',
+  git: 'https://github.com/djhi/meteor-autoform-moment-datepicker.git'
 });
 
 Package.onUse(function(api) {
-  api.use('templating@1.0.0');
-  api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0');
-  api.use('gildaspk:moment-datepicker@0.0.1');
+  api.use([
+    'templating@1.0.0',
+    'blaze@2.0.0',
+    'aldeed:autoform@5.4.0',
+    'gildaspk:moment-datepicker@0.0.3'
+  ]);
   api.addFiles([
     'autoform-moment-datepicker.html',
     'autoform-moment-datepicker.js'
